43881e5b571d7ecf74e0238e3b74bbb3017d7470
Josue Costa <contato_josuecosta@hotmail.com>
2025-01-15 17:01:02 -0400

    feat: estilizaçõa de section home

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+
+window.addEventListener("load", () => {
+    const spinner = document.querySelector(".spinner")
+    const contentMain = document.querySelectorAll(".containerBlock")
+
+
+    setTimeout(() => {
+        spinner.style.display = "none"
+        contentMain.forEach((e) => {
+            e.style.display = "flex"
+        })
+    }, 5000)
+})
