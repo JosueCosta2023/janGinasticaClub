cbf7d92b259003ea385e50315490fce2d5d41c5e
Josue Costa <contato_josuecosta@hotmail.com>
2025-01-24 16:00:48 -0400

    feat: conclusao de resposividade, criação de hamburguer button e menssagem de suceeso no formulario

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,12 @@
 window.addEventListener("load", () => {
-    const spinner = document.querySelector(".spinner")
+    const spinner = document.querySelector(".loader")
     const contentMain = document.querySelectorAll(".containerBlock")
+    const hamburguer = document.querySelector('.hamburguer');
+    const menu = document.querySelector('.menu');
+    const enviar = document.querySelector("#btEnviar")
+    const mensagemEmail = document.querySelector("#menssagem")
+    const frm = document.querySelector("form")
+
 
 
     setTimeout(() => {
@@ -19,6 +25,35 @@ window.addEventListener("load", () => {
     }, 3500)
 
     AOS.refresh()
+
+
+    // Adicionar evento de clique
+    hamburguer.addEventListener('click', () => {
+        hamburguer.classList.toggle('open'); // Alterna a classe do ícone
+        menu.classList.toggle('open'); // Alterna a classe do menu
+    });
+
+    enviar.addEventListener("click", () => {
+
+        const nome = frm.inName.value
+        const email = frm.inEmail.value
+
+        if (nome || email) {
+            // Exibir a mensagem
+            mensagemEmail.style.display = "block";
+
+            // Após 7 segundos, esconder a mensagem
+            setTimeout(() => {
+                mensagemEmail.style.display = "none";
+            }, 3000); // 7000 milissegundos = 7 segundos 
+        }
+
+        frm.inName.value = ""
+        frm.inName.focus()
+        frm.inEmail.value = ""
+
+
+    })
 })
 
 
